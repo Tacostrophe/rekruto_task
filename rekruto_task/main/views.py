d03ed20ab140e4b71481ec5fa17cb36009466af3
Дмитрий Кутин <TacoKong@yandex.ru>
2022-11-18 07:15:33 +0300

    Revert "task completed, i suppose"
    This reverts commit 24ff340af9314ffb64152fc64e12d69560dd4b78.

--- rekruto_task/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    if request.method == 'GET':
-        name = request.GET.get('name', 'Guest')
-        message = request.GET.get('message', 'Your message could be here')
-        return HttpResponse(f'Hello {name}! {message}!')
--- rekruto_task/rekruto_task/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rekruto_task.settings')
-
-application = get_wsgi_application()
